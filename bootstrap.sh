62d9074c5a0d026eb4774d3944b4f8e61850857b
Eric Moritz <eric@themoritzfamily.com>
2011-10-27 14:45:13 -0400

    changed the install root to /opt/ which it should of been in the first place

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 apt-get install git
-git clone git://github.com/ericmoritz/ubuntu-natty-web.git /var/ubuntu-natty-web
-cd /var/ubuntu-natty-web
+git clone git://github.com/ericmoritz/ubuntu-natty-web.git /opt/ubuntu-natty-web
+cd /opt/ubuntu-natty-web
 ./setup.sh
