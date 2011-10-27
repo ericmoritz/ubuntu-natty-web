4a51434f1087080b630c33a5630a82279464bc3a
Eric Moritz <eric@themoritzfamily.com>
2011-10-27 00:22:57 -0400

    Used the pypi version of uwsgi instead of someone's PPA

--- packages.sh
+++ packages.sh
@@ -2,13 +2,14 @@ apt-get install python-software-properties
 
 # Install all the ppa's
 add-apt-repository ppa:nginx/stable
-add-apt-repository ppa:uwsgi/release
+#add-apt-repository ppa:uwsgi/release
 
 # Refresh apt
 apt-get update
 
 # Install all the base packages
-apt-get install git nginx uwsgi redis-server memcached build-essential python-dev emacs python-setuptools git mercurial
+apt-get install git nginx redis-server memcached build-essential python-dev emacs python-setuptools git mercurial libxml2-dev
 easy_install pip virtualenv supervisor pastescript
+pip install uwsgi
 
 DATE=`date +%Y-%m-%dT%T`
